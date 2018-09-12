82600ec22d5589b0bcf006a92894731c55b5641d
Lena <lenaLatkovich@gmail.com>
2018-09-12 13:06:48 +0300

    add functions options

---
+++ git/Calculate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate {
+    class Program {
+
+        static float summary(float x, float y) {
+            return (x + y);
+        }
+
+        static float difference(float x, float y) {
+            return (x - y);
+        }
+
+        static float multiplication(float x, float y) {
+            return (x * y);
+        }
+
+        static float division(float x, float y) {
+            return (x / y);
+        }
+
+        static void Main(string[ ] args) {
+        }
+    }
+}
