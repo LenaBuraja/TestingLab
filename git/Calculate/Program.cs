758f8b894cd83323fc182fa1ffc0cad7cf0e6b82
Lena <lenaLatkovich@gmail.com>
2018-09-14 10:24:09 +0300

    add validation

--- git/Calculate/Program.cs
+++ git/Calculate/Program.cs
@@ -24,17 +24,37 @@ namespace Calculate {
         }
 
         static void Main(string[ ] args) {
-            float[ ] numbers = new float[10];
-            numbers[0] = 4;
-            numbers[1] = 6;
-            String operation = "*";
-            float result = 0;
-            if (operation == "*") result = multiplication(numbers[0], numbers[1]);
-            if (operation == "/" && numbers[1] != 0.0) result = division(numbers[0], numbers[1]);
-            if (operation == "+") result = summary(numbers[0], numbers[1]);
-            if (operation == "-") result = difference(numbers[0], numbers[1]);
-            Console.WriteLine(result);
-            Console.ReadLine( );
+            for (;;) {
+                Console.WriteLine("Enter expression:");
+                String mathExpression = Console.ReadLine( );
+                float[ ] numbers = new float[10];
+                int index = 0;
+                String number = "";
+                String operation = "a";
+                float result = 0;
+                for (int i = 0; i < mathExpression.Length; i++) {
+                    if (mathExpression[i].ToString( ) == "-" && number == "") {
+                        number = number.Insert(0, "-");
+                        continue;
+                    }
+                    if (mathExpression[i].ToString( ) == "/" || mathExpression[i].ToString( ) == "*" || mathExpression[i].ToString( ) == "-" || mathExpression[i].ToString( ) == "+" || mathExpression[i].ToString( ) == "=") {
+                        numbers[index] = (float)Convert.ToDouble(number);
+                        index++;
+                        operation = mathExpression[i].ToString( );
+                        number = "";
+                    } else {
+                        number += mathExpression[i].ToString( );
+                        if (i == mathExpression.Length - 1) numbers[index] = (float)Convert.ToDouble(number);
+
+                    }
+                }
+                Console.WriteLine(numbers[0] + " *** " + numbers[1] + " *** " + operation);
+                if (operation == "*") result = multiplication(numbers[0], numbers[1]);
+                if (operation == "/" && numbers[1] != 0.0) result = division(numbers[0], numbers[1]);
+                if (operation == "+") result = summary(numbers[0], numbers[1]);
+                if (operation == "-") result = difference(numbers[0], numbers[1]);
+                if (operation != "a") Console.WriteLine(result);
+            }
         }
     }
 }
