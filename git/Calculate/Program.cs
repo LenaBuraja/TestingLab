c0025fe6dfbc433b64942aeb845ee2995a24a954
Lena <lenaLatkovich@gmail.com>
2018-09-14 20:30:38 +0300

    add cicle

--- git/Calculate/Program.cs
+++ git/Calculate/Program.cs
@@ -24,6 +24,7 @@ namespace Calculate {
         }
 
         static void Main(string[ ] args) {
+            for (;;) {
                 Console.WriteLine("Enter expression:");
                 String mathExpression = Console.ReadLine( );
                 float[ ] numbers = new float[10];
@@ -53,6 +54,7 @@ namespace Calculate {
                 if (operation == "+") result = summary(numbers[0], numbers[1]);
                 if (operation == "-") result = difference(numbers[0], numbers[1]);
                 if (operation != "a") Console.WriteLine(result);
+            }
         }
     }
 }
