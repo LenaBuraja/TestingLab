52ec60ded47ac5e1ffee43b9fbf16d72d09e1d9a
Lena <lenaLatkovich@gmail.com>
2018-10-01 14:38:59 +0300

    rename method isCorrectTriagle in isTriagle

--- CorrectTriagle/CorrectTriagle/Program.cs
+++ CorrectTriagle/CorrectTriagle/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace CorrectTriagle {
     public class Program {
 
-        public static bool isCorrectTriagle(float a, float b, float c) {
+        public static bool isTriagle(float a, float b, float c) {
             if (float.IsNaN(a) || float.IsNaN(b) || float.IsNaN(c)) return false;
             if (a <= 0 || b <= 0 || c <= 0) return false;
             if ((float.IsInfinity(a) && float.IsInfinity(b)) ||
@@ -20,15 +20,15 @@ namespace CorrectTriagle {
         }
 
         static void Main(string[ ] args) {
-            bool triagle1 = isCorrectTriagle((float)6.3, (float)3.20000000000009, (float)9.5);
+            bool triagle1 = isTriagle((float)6.3, (float)3.20000000000009, (float)9.5);
             Console.WriteLine("6.3, (float)3.200000000000009, 9.5 - " + (triagle1 ? "is triagle" : "not triagle"));
-            bool triagle2 = isCorrectTriagle(3, 4, 2);
+            bool triagle2 = isTriagle(3, 4, 2);
              Console.WriteLine("3, 4, 2 - " + (triagle2 ? "is triagle" : "not triagle"));
-             bool triagle3 = isCorrectTriagle(5, 2, 2);
+             bool triagle3 = isTriagle(5, 2, 2);
              Console.WriteLine("5, 2, 2 - " + (triagle3 ? "is triagle" : "not triagle"));
-             bool triagle4 = isCorrectTriagle(2, 2, 2);
+             bool triagle4 = isTriagle(2, 2, 2);
              Console.WriteLine("2, 2, 2 - " + (triagle4 ? "is triagle" : "not triagle"));
-             bool triagle5 = isCorrectTriagle(float.PositiveInfinity, float.PositiveInfinity, 2);
+             bool triagle5 = isTriagle(float.PositiveInfinity, float.PositiveInfinity, 2);
             Console.ReadLine( );
         }
     }
--- CorrectTriagle/UnitTests/UnitTest1.cs
+++ CorrectTriagle/UnitTests/UnitTest1.cs
@@ -8,53 +8,53 @@ namespace UnitTests {
 
         [TestMethod] //1
         public void NegativeNumber( ) {
-            Assert.AreEqual(isCorrectTriagle(4, -2, 5), false);
+            Assert.AreEqual(isTriagle(4, -2, 5), false);
         }
 
         [TestMethod] //2
         public void TwoInfinity( ) {
-            Assert.AreEqual(isCorrectTriagle(float.PositiveInfinity, 4, float.PositiveInfinity), false);
+            Assert.AreEqual(isTriagle(float.PositiveInfinity, 4, float.PositiveInfinity), false);
         }
 
         [TestMethod] //3
         public void OneInfinity( ) {
-            Assert.AreEqual(isCorrectTriagle(4, float.PositiveInfinity, 5), false);
+            Assert.AreEqual(isTriagle(4, float.PositiveInfinity, 5), false);
         }
 
         [TestMethod] //4
         public void NotNAN() {
-            Assert.AreEqual(isCorrectTriagle(float.NaN, 4, 5), false);
+            Assert.AreEqual(isTriagle(float.NaN, 4, 5), false);
         }
 
         [TestMethod] //5
         public void Line( ) {
-            Assert.AreEqual(isCorrectTriagle(4, 3, 7), false);
+            Assert.AreEqual(isTriagle(4, 3, 7), false);
         }
 
         [TestMethod] //6
         public void FewerBorders( ) {
-            Assert.AreEqual(isCorrectTriagle(4, (float)2.99999, 7), false);
+            Assert.AreEqual(isTriagle(4, (float)2.99999, 7), false);
         }
 
         [TestMethod] //7
         public void MuchFewerBorders( ) {
-            Assert.AreEqual(isCorrectTriagle(1, 17, 23), false);
+            Assert.AreEqual(isTriagle(1, 17, 23), false);
         }
 
         [TestMethod] //8
         public void MoreBorders( ) {
-            Assert.AreEqual(isCorrectTriagle(4, (float)3.00009, 7), true);
+            Assert.AreEqual(isTriagle(4, (float)3.00009, 7), true);
         }
 
         [TestMethod] //9
         public void MuchMoreBorders( ) {
-            Assert.AreEqual(isCorrectTriagle(11, 17, 23), true);
+            Assert.AreEqual(isTriagle(11, 17, 23), true);
         }
 
 
         [TestMethod] //10
         public void EquilateralTriangle( ) {
-            Assert.AreEqual(isCorrectTriagle(14, 7, 14), true);
+            Assert.AreEqual(isTriagle(14, 7, 14), true);
         }
     }
 }
