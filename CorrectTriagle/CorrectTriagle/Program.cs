c6de3c649bce7a56d89aea3e42c49592bd3fa8c1
Lena <lenaLatkovich@gmail.com>
2018-10-01 13:34:12 +0300

    delete exception

--- CorrectTriagle/CorrectTriagle/Program.cs
+++ CorrectTriagle/CorrectTriagle/Program.cs
@@ -8,11 +8,11 @@ namespace CorrectTriagle {
     public class Program {
 
         public static bool isCorrectTriagle(float a, float b, float c) {
-            if (float.IsNaN(a) || float.IsNaN(b) || float.IsNaN(c)) throw new Exception("Must be number");
-            if (a <= 0 || b <= 0 || c <= 0) throw new Exception("Must be positive");
+            if (float.IsNaN(a) || float.IsNaN(b) || float.IsNaN(c)) return false;
+            if (a <= 0 || b <= 0 || c <= 0) return false;
             if ((float.IsInfinity(a) && float.IsInfinity(b)) ||
                 (float.IsInfinity(a) && float.IsInfinity(c)) ||
-                (float.IsInfinity(b) && float.IsInfinity(c))) throw new Exception("Uncertainty");
+                (float.IsInfinity(b) && float.IsInfinity(c))) return false;
             if (float.IsInfinity(a) || float.IsInfinity(b) || float.IsInfinity(c)) return false;
             if (a >= b && a >= c) return a < b + c;
             if (b >= a && b >= c) return b < a + c;
@@ -20,17 +20,15 @@ namespace CorrectTriagle {
         }
 
         static void Main(string[ ] args) {
-            bool triagle1 = isCorrectTriagle((float)6.0, (float)3.0000000000009, (float)9.0);
-            Console.WriteLine("6, (float)3.00000000000009, 9 - " + (triagle1 ? "is triagle" : "not triagle"));
-            bool triagle2 = isCorrectTriagle((float)6.3, (float)3.20000000000009, (float)9.5);
-            Console.WriteLine("6.3, (float)3.200000000000009, 9.5 - " + (triagle2 ? "is triagle" : "not triagle"));
-            /*bool triagle2 = isCorrectTriagle(3, 4, 2);
+            bool triagle1 = isCorrectTriagle((float)6.3, (float)3.20000000000009, (float)9.5);
+            Console.WriteLine("6.3, (float)3.200000000000009, 9.5 - " + (triagle1 ? "is triagle" : "not triagle"));
+            bool triagle2 = isCorrectTriagle(3, 4, 2);
              Console.WriteLine("3, 4, 2 - " + (triagle2 ? "is triagle" : "not triagle"));
              bool triagle3 = isCorrectTriagle(5, 2, 2);
              Console.WriteLine("5, 2, 2 - " + (triagle3 ? "is triagle" : "not triagle"));
              bool triagle4 = isCorrectTriagle(2, 2, 2);
              Console.WriteLine("2, 2, 2 - " + (triagle4 ? "is triagle" : "not triagle"));
-             bool triagle5 = isCorrectTriagle(float.PositiveInfinity, float.PositiveInfinity, 2);*/
+             bool triagle5 = isCorrectTriagle(float.PositiveInfinity, float.PositiveInfinity, 2);
             Console.ReadLine( );
         }
     }
--- CorrectTriagle/UnitTests/UnitTest1.cs
+++ CorrectTriagle/UnitTests/UnitTest1.cs
@@ -8,116 +8,53 @@ namespace UnitTests {
 
         [TestMethod] //1
         public void NegativeNumber( ) {
-            bool result;
-            try {
-                result = isCorrectTriagle(4, -2, 5);
-            }
-            catch (Exception e) {
-                Assert.AreEqual(e.Message, "Must be positive");
-                return;
-            }
-                 Assert.Fail("Exception expected");
+            Assert.AreEqual(isCorrectTriagle(4, -2, 5), false);
         }
 
         [TestMethod] //2
         public void TwoInfinity( ) {
-            bool result;
-            try {
-                result = isCorrectTriagle(float.PositiveInfinity, 4, float.PositiveInfinity);
-            } catch (Exception e) {
-                Assert.AreEqual(e.Message, "Uncertainty");
-                return;
-            }
-            Assert.Fail("Exception expected");
+            Assert.AreEqual(isCorrectTriagle(float.PositiveInfinity, 4, float.PositiveInfinity), false);
         }
 
         [TestMethod] //3
         public void OneInfinity( ) {
-            bool result;
-            try {
-                result = isCorrectTriagle(4, float.PositiveInfinity, 5);
-                Assert.AreEqual(result, false);
-           } catch (Exception e) {
-                Assert.Fail("Exception not expected");
-            }
+            Assert.AreEqual(isCorrectTriagle(4, float.PositiveInfinity, 5), false);
         }
 
         [TestMethod] //4
         public void NotNAN() {
-            bool result;
-            try {
-                result = isCorrectTriagle(float.NaN, 4, 5);
-            } catch (Exception e) {
-                Assert.AreEqual(e.Message, "Must be number");
-                return;
-            }
-            Assert.Fail("Exception expected");
+            Assert.AreEqual(isCorrectTriagle(float.NaN, 4, 5), false);
         }
 
         [TestMethod] //5
         public void Line( ) {
-            bool result;
-            try {
-                result = isCorrectTriagle(4, 3, 7);
-                Assert.AreEqual(result, false);
-            } catch (Exception e) {
-                Assert.Fail("Exception not expected");
-            }
+            Assert.AreEqual(isCorrectTriagle(4, 3, 7), false);
         }
 
         [TestMethod] //6
         public void FewerBorders( ) {
-            bool result;
-            try {
-                result = isCorrectTriagle(4, (float)2.99999, 7);
-                Assert.AreEqual(result, false);
-            } catch (Exception e) {
-                Assert.Fail("Exception not expected");
-            }
+            Assert.AreEqual(isCorrectTriagle(4, (float)2.99999, 7), false);
         }
 
         [TestMethod] //7
-        public void MoreBorders( ) {
-            bool result;
-            try {
-                result = isCorrectTriagle(4, (float)3.00009, 7);
-                Assert.AreEqual(result, true);
-            } catch (Exception e) {
-                Assert.Fail("Exception not expected");
-            }
+        public void MuchFewerBorders( ) {
+            Assert.AreEqual(isCorrectTriagle(1, 17, 23), false);
         }
 
         [TestMethod] //8
-        public void MuchMoreBorders( ) {
-            bool result;
-            try {
-                result = isCorrectTriagle(11, 17, 23);
-                Assert.AreEqual(result, true);
-            } catch (Exception e) {
-                Assert.Fail("Exception not expected");
-            }
+        public void MoreBorders( ) {
+            Assert.AreEqual(isCorrectTriagle(4, (float)3.00009, 7), true);
         }
 
         [TestMethod] //9
-        public void MuchFewerBorders( ) {
-            bool result;
-            try {
-                result = isCorrectTriagle(1, 17, 23);
-                Assert.AreEqual(result, false);
-            } catch (Exception e) {
-                Assert.Fail("Exception not expected");
-            }
+        public void MuchMoreBorders( ) {
+            Assert.AreEqual(isCorrectTriagle(11, 17, 23), true);
         }
+
 
         [TestMethod] //10
         public void EquilateralTriangle( ) {
-            bool result;
-            try {
-                result = isCorrectTriagle(14, 7, 14);
-                Assert.AreEqual(result, true);
-            } catch (Exception e) {
-                Assert.Fail("Exception not expected");
-            }
+            Assert.AreEqual(isCorrectTriagle(14, 7, 14), true);
         }
     }
 }
